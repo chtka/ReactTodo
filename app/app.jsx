5018111ddd1de3e1a7e179a3944089af44304da1
Ben Lalanes <chtka.ow@gmail.com>
2017-12-09 13:44:06 -0800

    react boilerplate

---
+++ app/app.jsx
@@ -0,0 +1,18 @@
+// presentational component: simply renders information
+// container component: maintains state and renders children
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+
+// Load foundation
+require('style!css!foundation-sites/dist/foundation.min.css');
+$(document).foundation();
+
+// app css
+require('style!css!sass!applicationStyles');
+
+// when path is "about", renders about, if its just the index of the page, then renders weather
+ReactDOM.render(
+    <p>Boilerplate project</p>,
+    document.getElementById('app')
+);
